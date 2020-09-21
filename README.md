d3f64032d0f3f054a676769b6d5d3dbbff719424
Shlomo Kashani <shlomo@bayesian.io>
2020-09-21 14:19:07 +0300

    First

--- README.md
+++ README.md
@@ -14,7 +14,6 @@
   <img src="https://github.com/QuantScientist/PngTorch/blob/master/asstes/logo.png?raw=true"></a>
 </h1>
 
-
 ---
 
 ## About
@@ -57,7 +56,33 @@ the tensor as an PNG image on the file system.
 Full source code:
 
 ```cpp
+//#include <torch/torch.h>
+#include <torch/script.h> // One-stop header.
+#include <iostream>
+#include <iostream>
+#include <chrono>
+#include <iostream>
+#include <typeinfo>
+#include <thread>
+#include <future>
+#include "../include/utils/vision_utils.hpp"
+using namespace std;
+using namespace std::chrono;
 
+int main(int argc, char *argv[]) { 
+    auto VU= VisionUtils();
+    torch::Device device(torch::kCUDA );
+    // Input PNG-image
+    png::image<png::rgb_pixel> imageI("clion.png");
+    // Convert png::image into torch::Tensor
+    torch::Tensor tensor = VU.pngToTorch(imageI, device); //Note: we are allocating on the GPU
+    std::cout << "C:" << tensor.size(0) << " H:" << tensor.size(1) << " W:" << tensor.size(2) << std::endl;
+    // Convert torch::Tensor into png::image
+    png::image<png::rgb_pixel> imageO = VU.torchToPng(tensor);
+    // Input PNG-image
+    imageO.write("clion-output001.png");
+    return 0;
+}
 ```
 
 
--- src/neural.cpp
+++ src/neural.cpp
@@ -1,14 +1,8 @@
 //#include <torch/torch.h>
 #include <torch/script.h> // One-stop header.
-
 #include <iostream>
-#include <vector>
-#include <memory>
-#include <cmath>
-
 #include <iostream>
 #include <chrono>
-
 #include <iostream>
 #include <typeinfo>
 #include <thread>
@@ -16,18 +10,10 @@
 #include "../include/utils/vision_utils.hpp"
 using namespace std;
 using namespace std::chrono;
-#define kIMAGE_SIZE 512
-#define kCHANNELS 3
-#define kTOP_K 3
 
-//temporarily disable gradient recording
-//NoGradGuard guard;
-
-
-int main(int argc, char *argv[]) { //./../style_model_cpp.pt ./../imgs/RGB_00_001.jpg
+int main(int argc, char *argv[]) {
     auto VU= VisionUtils();
     torch::Device device(torch::kCUDA );
-
     // Input PNG-image
     png::image<png::rgb_pixel> imageI("clion.png");
     // Convert png::image into torch::Tensor
@@ -37,17 +23,5 @@ int main(int argc, char *argv[]) { //./../style_model_cpp.pt ./../imgs/RGB_00_00
     png::image<png::rgb_pixel> imageO = VU.torchToPng(tensor);
     // Input PNG-image
     imageO.write("clion-output001.png");
-
-//    auto videoLambda =[&](const std::string&  modelName, c10::Device device, const std::string&  vidName) {
-//        VU.processVideo(modelName, device, vidName);
-//    };
-//
-//    // Loading your model
-//    const std::string s_model_name0 = "style_model_cpp.pt";
-//    const std::string s_model_name1 = "erfnet_fs.pt";
-//    const std::string s_model_name2 = "vgg19_layers.pt"; // Forward= D=:[1, 512, 22, 40]
-//    auto vid1Callback2 =std::async(std::launch::async, [&](){
-//        videoLambda(s_model_name0, device,  s_image_name0);
-//    });
     return 0;
 }
